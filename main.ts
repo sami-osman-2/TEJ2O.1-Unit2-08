a099183709629d2202c20799b5cb1276660822df
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-29 14:55:40 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sami Osman
+ * Created on: Nov 2025
+ * This program allows pins 13 through 15 to be used for RGB LED
 */
 
-basic.showString('Hello, World!')
+let red = DigitalPin.P13
+let green = DigitalPin.P14
+let blue = DigitalPin.P15
+
+// Helper function
+function showColor(r: number, g: number, b: number) {
+    pins.digitalWritePin(red, r)
+    pins.digitalWritePin(green, g)
+    pins.digitalWritePin(blue, b)
+}
+
+basic.forever(function () {
+    if (input.buttonA.isPressed()) {
+
+        // Red
+        showColor(1, 0, 0)
+        basic.pause(1000)
+
+        // Green
+        showColor(0, 1, 0)
+        basic.pause(1000)
+
+        // Blue
+        showColor(0, 0, 1)
+        basic.pause(1000)
+
+        // Off
+        showColor(0, 0, 0)
+    }
+})
+
