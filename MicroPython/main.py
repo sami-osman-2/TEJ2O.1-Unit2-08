79ca05bd911811eb619525a4c949b829e4d282c8
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-29 14:53:59 -0500

    Update main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,38 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Sami Osman
+Created on: Nov 2025
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+# assign pins to colours
+red = pin13
+green = pin14
+blue = pin15
 
-display.scroll("Hello, World!")
+
+# colour setters
+def show_colour(r, g, b):
+    red.write_digital(r)
+    green.write_digital(g)
+    blue.write_digital(b)
+
+
+while True:
+    if button_a.is_pressed():
+
+        # Red
+        show_colour(1, 0, 0)
+        sleep(1000)
+
+        # Green
+        show_colour(0, 1, 0)
+        sleep(1000)
+
+        # Blue
+        show_colour(0, 0, 1)
+        sleep(1000)
+
+        # Turn off LED
+        show_colour(0, 0, 0)
